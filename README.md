ec68f487b45052d9f49a2619171f4cc179d143de
David Andersson <lejonmanen@gmail.com>
2022-05-03 10:58:13 +0200

    Readme and movie data

---
+++ README.md
@@ -0,0 +1,13 @@
+# Hollywood watch
+
+Ett demo av en React-app med flera komponenter.
+
+Du kan titta på commit-historiken för att se vilka steg vi gick igenom.
+
+Byggt med Vite. För att köra:
+```
+git clone [repots URL]
+cd 07-hollywood-watch/
+npm install
+npm run dev
+```
--- src/components/grid/movieData.ts
+++ src/components/grid/movieData.ts
@@ -2,9 +2,24 @@ import { Movie } from '../../models/Movie'
 
 export const testData: Movie[] = [
 	{
-		id: 1,
-		title: 'My neighbour Totoro',
-		imageUrl: '',
-		premiereDate: '2021-09-28'
+		id: 1, title: 'My neighbour Totoro', imageUrl: 'https://media.ginza.se/Images/item_img_1200/315352.jpg', premiereDate: '1988-04-16'
+	},
+	{
+		id: 2, title: 'Princess Mononoke', imageUrl: 'https://media.ginza.se/Images/item_img_1200/328204.jpg', premiereDate: '1997-07-12'
+	},
+	{
+		id: 5, title: 'Spirited Away', imageUrl: 'https://m.media-amazon.com/images/M/MV5BMjlmZmI5MDctNDE2YS00YWE0LWE5ZWItZDBhYWQ0NTcxNWRhXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg', premiereDate: '2001-07-20'
+	},
+	{
+		id: 8, title: 'Star Wars: A new hope', imageUrl: 'https://www.kaptenkrok.se/media/catalog/product/cache/1/image/317x450/9df78eab33525d08d6e5fb8d27136e95/d/v/dvd-s-4432.jpg', premiereDate: '1977-12-16'
+	},
+	{
+		id: 9, title: 'Star Wars: The empire strikes back', imageUrl: 'https://cdn.abicart.com/shop/4862/art62/1021156/1021156-origpic-f3db72.jpg', premiereDate: '1980-08-15'
+	},
+	{
+		id: 10, title: 'Star Wars: Return of the Jedi', imageUrl: 'https://d2iltjk184xms5.cloudfront.net/uploads/photo/file/278385/small_76076d2104bad5a1e839e267a0a88ae7-jedins_aterkomst_97.jpg', premiereDate: '1983-09-30'
+	},
+	{
+		id: 9001, title: 'The Shawshank Redemption', imageUrl: 'https://images-na.ssl-images-amazon.com/images/S/pv-target-images/9964546b0ba1f6e14a6045e34b341f8ca2a3569752c5afed95b89682fcde1a68._RI_V_TTW_.jpg', premiereDate: '1995-03-03'
 	},
 ]
